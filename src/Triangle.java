02798d60b3d2ffd67938e23d7d44638cbb94482b
Olexander Zhurba <starkyzh@gmail.com>
2023-05-04 16:29:39 +0200

    solve Task with an example

---
+++ src/Triangle.java
@@ -0,0 +1,41 @@
+public class Triangle {
+
+  private int a;
+  private int b;
+  private int c;
+
+  public Triangle(int a, int b, int c) {
+    this.a = checkOneSide(a);
+    this.b = checkOneSide(b);
+    this.c = checkOneSide(c);
+    if (!checkSides(a, b, c)) {
+      String message = "Sides are unreal, sum of two some sides must be great as third side";
+      throw new IllegalArgumentException(message);
+    }
+  }
+
+  public int getA() {
+    return a;
+  }
+
+  public int getB() {
+    return b;
+  }
+
+  public int getC() {
+    return c;
+  }
+
+  private int checkOneSide(int side) {
+    if (side < 0) {
+      throw new IllegalArgumentException("Side doesn't be negative: " + side);
+    } else {
+      return side;
+    }
+  }
+
+  public static boolean checkSides(int a, int b, int c) {
+    return ((a + b) > c && (a + c) > b && (b + c) > a);
+  }
+
+}
